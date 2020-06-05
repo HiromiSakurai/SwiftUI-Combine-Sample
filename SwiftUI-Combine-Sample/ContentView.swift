7bc1051873a388a242dc1e283e2fc1ccbf925e33
HiromiSakurai <sakurai465270@gmail.com>
2020-06-05 14:55:05 +0900

    Add new project

---
+++ SwiftUI-Combine-Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Combine-Sample
+//
+//  Created by 櫻井寛海 on 2020/06/05.
+//  Copyright © 2020 HiromiSakurai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
